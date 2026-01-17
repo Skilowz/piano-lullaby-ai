2512ebd3638a0560d9c15f09a09f7ed05de7ea3f
Skilowz <160808424+Skilowz@users.noreply.github.com>
2026-01-17 13:23:59 -0300

    Update phrase_detection.py

--- perception/phrase_detection.py
+++ perception/phrase_detection.py
@@ -1,1 +1,7 @@
+import librosa
+import numpy as np
 
+def detect_phrases(y, sr):
+    rms = librosa.feature.rms(y=y)[0]
+    boundaries = np.where(rms < np.percentile(rms, 20))[0]
+    return boundaries.tolist()
