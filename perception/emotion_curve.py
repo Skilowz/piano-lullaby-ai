b1f844ad4638a7f5db1475745ac9901497b68c57
Skilowz <160808424+Skilowz@users.noreply.github.com>
2026-01-17 13:24:33 -0300

    Update emotion_curve.py

--- perception/emotion_curve.py
+++ perception/emotion_curve.py
@@ -1,1 +1,10 @@
+import librosa
+import numpy as np
 
+def extract_emotion_curve(y, sr):
+    chroma = librosa.feature.chroma_stft(y=y, sr=sr)
+    energy = librosa.feature.rms(y=y)[0]
+    return {
+        "energy_mean": float(np.mean(energy)),
+        "energy_curve": energy.tolist()
+    }
